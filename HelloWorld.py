efe241d443e829ead0afb798e77d0ac23f301517
cserajeevdas <33643923+cserajeevdas@users.noreply.github.com>
2017-11-14 07:42:49 +0530

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,23 @@
+# print('Hello, World')
+# print(20+30)
+# print(30/20)
+# print(40*50)
+# print( )
+# print("End")
+# print('I am "Rajeev" Das' )
+# print("Hello" + " world")
+# greeting = "Hello"
+# name = "Sneha!"
+#displaying greetings
+# print( greeting  + '  '+ name)
+#take user input and display
+# greeting = "Hello!"
+# name = input("Please enter your name:")
+# print(greeting + ' ' + name)
+#split a string
+# splittabbedstring = "I live in \n Pune for \t three years"
+# print(splittabbedstring)
+# anotherSplitString = """ Hello I am
+# from
+# Pune"""
+# print(anotherSplitString)
