d48cc7e41741529cf5b255cce91b996a2e554574
cserajeevdas <33643923+cserajeevdas@users.noreply.github.com>
2020-07-06 18:40:29 +0530

    Create if_condition.py

---
+++ if_condition.py
@@ -0,0 +1,10 @@
+val = False
+def main(val):
+    if True==val:
+        print("true")
+    elif False==val:
+        print("false")
+    else:
+        print("nothing else")
+if __name__=="__main__":
+    main(val)
