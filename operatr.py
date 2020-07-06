581f1e87a3b8bff09655e174b79ff2a5a8925d87
cserajeevdas <33643923+cserajeevdas@users.noreply.github.com>
2020-07-06 18:39:58 +0530

    Create operatr.py

---
+++ operatr.py
@@ -0,0 +1,20 @@
+x=6
+y=3
+z=x+y
+#Addition
+print(f'x + y is: {z}')
+
+#Interger divison
+z= x//y
+print(f'x divided y is: {z}')
+
+#Modulo
+z= x%y
+print(f'x modulo y is: {z}')
+
+#Negate
+x= -x
+print("negate of x is", x)
+x=+x
+
+#Supports other operators too like <, >, =<, >=, ==, !=
